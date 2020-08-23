52fda127d6a799b2657777aa428c618a566689af
Hector <hectorfly21@gmail.com>
2020-08-23 17:12:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,8 +15,7 @@ end
  [2,4,6]#right diagonal
 ]
 def won?(board)
-for each win_combination in
-WIN_COMBINATIONS
+  WIN_COMBINATIONS.each do |win_combination|
  win_index_1 = win_combination[0]
  win_index_2 = win_combination[1]
  win_index_3 = win_combination[2]
