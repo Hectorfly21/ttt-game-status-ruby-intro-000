332738275f64afe1660ddb6a8ea70eb5eb57d28b
Hector <hectorfly21@gmail.com>
2020-08-23 17:53:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,3 +37,13 @@ end
    return true
   end
 end
+
+def draw(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif !full(board) && !won(board)
+    return false
+  else won?(board)
+    return false 
+  end
+end
