1989c1ce93761f9c9d34295dfa5f68baf3dbf138
Hector <hectorfly21@gmail.com>
2020-08-23 15:59:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,8 +4,8 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-def WIN_COMBINATIONS(constant)
-  WIN_COMBINATIONS.class = [
+def WIN_COMBINATIONS(board)
+  WIN_COMBINATIONS.constant = [
  [0,1,2],
  [3,4,5],
  [6,7,8],
